2f84c84ff53df1b081ee29aa8b3a0f27f45e6743
priscahlimo <priscahlimoh@gmail.com>
2022-03-12 14:26:41 +0300

    add constructor,getter and setter methods to the sightings class

--- src/main/java/Models/Sightings.java
+++ src/main/java/Models/Sightings.java
@@ -9,4 +9,37 @@ public class Sightings {
     private String location;
     private String rangerName;
     private Timestamp lastSeen;
+
+
+
+    public Sightings (int animalId, String location, String rangerName){
+        this.animalId = animalId;
+        this.location = location;
+        this.rangerName = rangerName;
+
+        if (location.isEmpty() || rangerName.isEmpty()){
+            throw new IllegalArgumentException("Please enter all input fields.");
+        }
+    }
+
+    public int getAnimalId() {
+        return animalId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getRangerName() {
+        return rangerName;
+    }
+
+    public Timestamp getLastSeen() {
+        return lastSeen;
+    }
+
 }
