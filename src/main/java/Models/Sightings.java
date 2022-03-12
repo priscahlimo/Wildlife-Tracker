592d1f30f3f98dec274b3e190847a661acf2bcca
priscahlimo <priscahlimoh@gmail.com>
2022-03-12 14:25:19 +0300

    add animal sightings properties in the class

--- src/main/java/Models/Sightings.java
+++ src/main/java/Models/Sightings.java
@@ -1,4 +1,12 @@
 package Models;
 
+import java.sql.Timestamp;
+
 public class Sightings {
+
+    private int animalId;
+    private int id;
+    private String location;
+    private String rangerName;
+    private Timestamp lastSeen;
 }
