6609b0f13cea4efc8f73e03bc49edae7403fced0
priscahlimo <priscahlimoh@gmail.com>
2022-03-13 15:01:20 +0300

    update DB

--- src/main/java/Models/DB.java
+++ src/main/java/Models/DB.java
@@ -1,8 +1,36 @@
 package Models;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.sql2o.Sql2o;
 
-public class DB {
-       public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", "moringa", "briellepriscah");
+import java.net.URI;
+import java.net.URISyntaxException;
 
+public class DB {
+       private static URI dbUri;
+       public static Sql2o sql2o;
+
+       static {
+              Logger logger = LoggerFactory.getLogger(DB.class);
+
+              try {
+                     if (System.getenv("DATABASE_URL") == null) {
+                            dbUri = new URI("postgres://localhost:5432/wildlife_tracker");
+                            logger.info("Using local database.");
+                     } else {
+                            dbUri = new URI(System.getenv("DATABASE_URL"));
+                     }
+
+                     int port = dbUri.getPort();
+                     String host = dbUri.getHost();
+                     String path = dbUri.getPath();
+                     String username = (dbUri.getUserInfo() == null) ? "moringa" : dbUri.getUserInfo().split(":")[0];
+                     String password = (dbUri.getUserInfo() == null) ? "briellepriscah" : dbUri.getUserInfo().split(":")[1];
+
+                     sql2o = new Sql2o("jdbc:postgresql://" + host + ":" + port + path, username, password);
+              } catch (URISyntaxException e ) {
+                     logger.error("Unable to connect to database.");
+              }
+       }
 }
